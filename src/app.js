5d1b46ea07c9fe182cdeeca09cc85a70c07ab7cd
LaxOn <br_lacson@yahoo.com>
2020-11-14 13:16:40 -0500

    created a data access library

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
 const path = require('path')
 const express = require('express')
+const { accounts, users, writeJSON } = require('./data.js')
 
 const app = express()
 
@@ -9,12 +10,6 @@ app.set()
 app.set('view engine', 'ejs')
 app.use(express.static(path.join(__dirname, '/public')))
 app.use(express.urlencoded({ extended: true }))
-
-const accountData = fs.readFileSync(path.join(__dirname, '/json/accounts.json'), { encoding: 'utf8' })
-const accounts = JSON.parse(accountData)
-
-const userData = fs.readFileSync(path.join(__dirname, '/json/users.json'), { encoding: 'utf8' })
-const users = JSON.parse(userData)
 
 app.get('/', (req, res) => {
   res.render(
@@ -76,8 +71,7 @@ app.get('/payment', (req, res) => {
 app.post('/payment', (req, res) => {
   accounts.credit.balance -= parseInt(req.body.amount)
   accounts.credit.available += parseInt(req.body.amount)
-  const accountsJSON = JSON.stringify(accounts)
-  fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8')
+  writeJSON()
   res.render(
     'payment',
     {
@@ -89,8 +83,7 @@ app.post('/payment', (req, res) => {
 app.post('/transfer', (req, res) => {
   accounts[req.body.from].balance = parseInt(accounts[req.body.from].balance) - parseInt(req.body.amount)
   accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount)
-  const accountsJSON = JSON.stringify(accounts)
-  fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8')
+  writeJSON()
   res.render('transfer', { message: 'Transfer Completed' })
 })
 
